462961de7b53b2dbe11d2e7c76522f21d4c6e6e8
Jubayer husain <jubayerhusai360@gmail.com>
2024-11-16 19:20:49 +0600

    find us on section done

--- src/LayoutComponent/RightAside.jsx
+++ src/LayoutComponent/RightAside.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { FaGithub } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
 function RightAside() {
   return (
     <div>
-        {/* login with provider */}
+      {/* login with provider */}
       <div className="text-center space-y-3 mt-5">
         <button className="btn w-full border-blue-300 text-lg font-semibold flex items-center">
           <FcGoogle></FcGoogle>
@@ -15,6 +15,21 @@ function RightAside() {
           <FaGithub></FaGithub>
           <p>Login with Github</p>
         </button>
+      </div>
+      {/* find us on section */}
+      <div className="mt-3 space-y-3">
+      <h2 className="font-semibold text-xl mb-3">Find Us On</h2>
+      <div className="join flex join-vertical *:bg-base-100">
+        <button className="btn join-item justify-start text-lg font-semibold text-gray-600">
+          <FaFacebook></FaFacebook> Facebook
+        </button>
+        <button className="btn join-item justify-start text-lg font-semibold text-gray-600">
+          <FaInstagram></FaInstagram> Instagram
+        </button>
+        <button className="btn join-item justify-start text-lg font-semibold text-gray-600">
+          <FaTwitter></FaTwitter> Twitter
+        </button>
+      </div>
       </div>
     </div>
   );
