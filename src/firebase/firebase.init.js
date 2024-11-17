db96392c3d8b62b5c8cf8e21edf61ce7ff751ec5
Jubayer husain <jubayerhusai360@gmail.com>
2024-11-17 14:06:45 +0600

    some update

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -6,14 +6,23 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  // apiKey: import.meta.env.VITE_apiKey,
+  // authDomain: import.meta.env.VITE_authDomain,
+  // projectId: import.meta.env.VITE_projectId,
+  // storageBucket: import.meta.env.VITE_storageBucket,
+  // messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  // appId: import.meta.env.VITE_appId,
+  apiKey: "AIzaSyDpTDNQX_OSLd5ejZsEh7SPqPp50wqlOnI",
+  authDomain: "dragon-news-f20de.firebaseapp.com",
+  projectId: "dragon-news-f20de",
+  storageBucket: "dragon-news-f20de.firebasestorage.app",
+  messagingSenderId: "163158446567",
+  appId: "1:163158446567:web:212627b508cfb19c1dad42"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
+
+
+
